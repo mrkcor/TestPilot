92f73e5abbe15a3ac3fa89851caf3ded6eb7d6e2
Mark Kremer <mark@5ec.nl>
2014-07-26 22:11:21 +0200

    Copy rubygems when building

--- src/testpilot/core/ScriptRunner.java
+++ src/testpilot/core/ScriptRunner.java
@@ -10,6 +10,7 @@ public class ScriptRunner {
 
     public void run(String script) {
         ScriptingContainer container = new ScriptingContainer();
+        // FIXME: Beter manier om het basepath te bepalen
         String basepath = System.getProperty("user.dir");
         container.runScriptlet("ENV['GEM_PATH']='" + basepath + "/lib/rubygems/'");
         // TODO: Connect container STDOUT and STDERR to GUI
