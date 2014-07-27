051f23ce750b8b397071fe7cd4c6920e57d74bf3
Mark Kremer <mark@5ec.nl>
2014-07-27 11:45:39 +0200

    Set Ruby script container to a Singleton

--- src/testpilot/core/ScriptRunner.java
+++ src/testpilot/core/ScriptRunner.java
@@ -1,5 +1,6 @@
 package testpilot.core;
 
+import org.jruby.embed.LocalContextScope;
 import org.jruby.embed.ScriptingContainer;
 
 /**
@@ -10,7 +11,7 @@ public class ScriptRunner {
 
     public void run(String script) {
         // NOTE: With LocalContextScope.SINGLETHREAD as argument you get a clean interpreter, it will be slower though
-        ScriptingContainer container = new ScriptingContainer();
+        ScriptingContainer container = new ScriptingContainer(LocalContextScope.SINGLETON);
         // FIXME: A better way is needed to determine the working directory
         String basepath = System.getProperty("user.dir");
         container.runScriptlet("ENV['GEM_PATH']='" + basepath + "/lib/rubygems/'");
