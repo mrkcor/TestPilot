51fb4695cfb09e85b29944dcfcb97f1a1b467884
Mark Kremer <mark@5ec.nl>
2014-07-31 22:10:08 +0200

    Remove license texts from individual source files

--- src/testpilot/core/TestPilot.java
+++ src/testpilot/core/TestPilot.java
@@ -1,26 +1,3 @@
-/*
- * The MIT License
- *
- * Copyright 2014 Mark Kremer.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package testpilot.core;
 
 import java.io.File;
@@ -30,10 +7,6 @@ import java.util.ArrayList;
 import org.jruby.embed.LocalContextScope;
 import org.jruby.embed.ScriptingContainer;
 
-/**
- *
- * @author Mark Kremer
- */
 public class TestPilot {
 
     private static TestPilot instance;
